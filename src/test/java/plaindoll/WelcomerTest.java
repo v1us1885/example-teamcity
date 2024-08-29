a1abfcebc08e0229560e88e7396a52069ec7682d
v1us1885 <v1rus1885@mail.ru>
2024-08-29 11:54:28 +0300

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,8 +30,4 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
-	@Test
-	public void welcomerGetsayHunter(){
-		assertThat(welcomer.GetsayHunter(), containsString("hunter"));
-	}
 }
