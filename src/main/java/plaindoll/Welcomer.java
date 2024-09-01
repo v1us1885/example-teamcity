89b1e9ab7dc668e43af1c658704a6fdd1efc647d
v1us1885 <62296189+v1us1885@users.noreply.github.com>
2024-09-01 12:14:13 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-	public String GetsayHunter(){
+	public String sayHunter(){
 		return "That's the word hunter";
 	}	
 }
